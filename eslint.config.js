23ec5dc88684842cc5df98c25a395319da89719f
Michael Frank <m_frank84@outlook.de>
2025-11-10 23:03:55 +0100

    update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const prettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  [
+    {
+      files: ["**/*.ts"],
+      extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...tseslint.configs.stylistic,
+        ...angular.configs.tsRecommended,
+      ],
+      processor: angular.processInlineTemplates,
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+      },
+    },
+    {
+      // Any project level overrides or additional rules for TypeScript files can go here
+      // (we don't need to extend from any typescript-eslint or angular-eslint configs because
+      // we already applied the rootConfig above which has them)
+      files: ['**/*.ts'],
+      extends: [prettierRecommended], // here we inherit from the recommended setup from eslint-plugin-prettier for TS
+      rules: {},
+    },
+    {
+      // Any project level overrides or additional rules for HTML files can go here
+      // (applies to both external template files AND inline templates)
+      // (we don't need to extend from any angular-eslint configs because
+      // we already applied the rootConfig above which has them)
+      files: ['**/*.html'],
+      extends: [prettierRecommended], // here we inherit from the recommended setup from eslint-plugin-prettier for HTML
+      rules: {},
+    }
+  ],
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
